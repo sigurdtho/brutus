bc3d864fe854670c9e5fcf7349af2817473cef71
Simon Fagerli <fagerli93@live.no>
2017-10-02 15:25:49 +0200

    Added default path to go to livfeed

--- brutus/brutus/urls.py
+++ brutus/brutus/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^liveupdates/', include('liveupdates.urls')),
+    url(r'^$', include('liveupdates.urls')),
 ]
